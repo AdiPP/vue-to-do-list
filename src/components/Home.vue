333a1c9ad46717070f406fcc067a1ec8ffee8398
Adi Putra <adiputrapermana@gmail.com>
2021-03-29 05:45:09 +0700

    feature(to-do-list): introduce to do list feature

---
+++ src/components/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <form @submit.prevent="add">
+      <input type="text" v-model="newItem">
+      <button type="submit">Add</button>
+    </form>
+    <items :items="items" :remove="remove"></items>
+  </div>
+</template>
+
+<script>
+import items from "@/components/Items";
+
+export default {
+  components: {
+    items
+  },
+  data() {
+    return {
+      lists: [
+        {'name': 'Adi'},
+        {'name': 'Permana'},
+        {'name': 'Putra'},
+        {'name': 'Vuejs'},
+        {'name': 'Angular'},
+        {'name': 'Pikachu'}
+      ],
+      newItem: null
+    }
+  },
+  computed: {
+    items: function () {
+      return this.lists.filter(function (item) {
+        if (item.name === 'React') item.name = 'No React Allowed';
+
+        return item;
+      })
+    }
+  },
+  methods: {
+    remove(index) {
+      this.lists.splice(index,1);
+    },
+    add() {
+      if (null !== this.newItem) {
+        this.lists.push({
+          'name': this.newItem
+        })
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Items.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ul>
+      <li
+          v-for="(item, index) in items"
+          :key="item.name"
+      >
+        {{item.name}} <button @click="removeItem(index)">X</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+ export default {
+   props: {
+     items: Array,
+     remove: Function
+   },
+   methods: {
+     removeItem(index) {
+       this.remove(index);
+     }
+   }
+ }
+</script>
+
+<style>
+
+</style>
